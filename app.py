512a104bd1a4da774f4bc0839f600f7c74920983
Felix <felixsavero78@gmail.com>
2024-05-19 18:49:56 +0700

    fix: fix preprocessing

--- app.py
+++ app.py
@@ -13,25 +13,32 @@ app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
 def validate_file(filename: str):
   return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-# Function to classify image
-def classify_image(image_path: str):
+# Preprocess images
+def preprocess_image(image_path: str, target_size=(224, 224)):
+  image = cv2.imread(image_path)
+  image = cv2.resize(image, target_size, interpolation=cv2.INTER_AREA)
   clahe = cv2.createCLAHE(clipLimit=1, tileGridSize=(8, 8))
-  image = cv2.resize(cv2.imread(image_path), (224, 224), interpolation=cv2.INTER_AREA)
   r_channel, g_channel, b_channel = cv2.split(image)
   enhanced_r = clahe.apply(r_channel)
   enhanced_g = clahe.apply(g_channel)
   enhanced_b = clahe.apply(b_channel)
   image = cv2.merge((enhanced_r, enhanced_g, enhanced_b))
+  image = image.astype(np.float32) / 255.0
   image = np.expand_dims(image, axis=0)
+  return image
+
+# Function to classify image
+def classify_image(image_path: str):
   try:
+    image = preprocess_image(image_path)
     model_path = os.path.join("ml-model", "InceptionV3-Dermnet-SMOTE-Minority-LR-0.001-Batch Size 16.h5")
     model = load_model(model_path)
     result = model.predict(image)
-    print(result)
-    return str(result)
+    class_labels = ["Mild", "Moderate", "Severe"]
+    return str(class_labels[np.argmax(result)])
   except Exception as e:
-    print(e)
-    print("ERROR")
+    print("Error loading or predicting with model:", e)
+    return "Error"
 
 # Routes
 @app.route('/', methods=['GET', 'POST'])
